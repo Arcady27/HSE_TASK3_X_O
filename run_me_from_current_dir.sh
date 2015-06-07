1c1b3575f2139c90ac7d8b66670af4a0e2ca643b
Arcady27 <arcady27@gmail.com>
2015-06-07 23:49:00 +0300

    config

--- run_me_from_current_dir.sh
+++ run_me_from_current_dir.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Navigate your browser to http://localhost:8090/"
+echo "Navigate your browser to http://localhost:8088/"
 erl -noshell -pa ./ebin -s logic_server -s inets -config my_server
 
